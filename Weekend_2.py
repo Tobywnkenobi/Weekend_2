73d855fe197f73329b58e07e7c61f9928285d06d
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-09-01 14:27:57 -0400

    first commit

---
+++ Weekend_2.py
@@ -0,0 +1,34 @@
+# Class Methods
+# Your Parking Garage class should implement the following methods:
+
+# - take_ticket:
+#   - Decrease the number of available tickets and parking spaces by 1.
+# - pay_for_parking:
+#   - Show an input prompt asking for the payment amount.
+#   - If the payment variable is not empty, display a message that the ticket has been paid and they have 15 minutes to leave.
+#   - Update the `current_ticket` dictionary, setting the key "paid" to True.
+ 
+# - leave_garage:
+#   - If the ticket has been paid, display a "Thank You, have a nice day" message.
+#   - If not, prompt for payment.
+#   - Once paid, update the parking spaces and tickets lists to increase by 1.
+
+# Attributes
+# You will need the following attributes:
+# - `tickets` (list)
+# - `parking_spaces` (list)
+# - `current_ticket` (dictionary)
+
+# Environment & Accountability
+# Use VSCode for this project and start with the files linked below. Each person should document the portion of the project they contributed to within the Python file and the README.
+
+# Learning Objectives
+# By the end of this project, each group/student should be able to:
+# - Demonstrate proficiency in using Git and GitHub for collaboration.
+# - Understand the concept of classes and object-oriented programming.
+# - Create class methods and understand their utility.
+# - Demonstrate how to instantiate classes.
+
+# Final Steps
+# Upon project completion, commit the final changes, synchronize all pull requests, and submit your GitHub links. The code should be identical across all members of the team.
+
