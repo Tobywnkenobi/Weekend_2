8b9597a90b5d94d8fd615cef3edab41281be79e7
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-09-02 01:28:34 -0400

    last one for the night

--- Weekend_2.py
+++ Weekend_2.py
@@ -41,40 +41,48 @@ current_ticket = {}
 
 class ParkingGarage:
     def __init__(self, total_tickets=100,total_spaces=100):
-        self.ticket = [i for i in range(1, total_tickets +1)]
+        self.tickets = [i for i in range(1, total_tickets +1)]
         self.parking_spaces = [i for i in range(1, total_spaces +1)]
         self.current_ticket = {}         
         
-def Take_ticket(self):
-    if len(self.tickets) >0 and len(self.parking_spaces) > 0:
-        ticket = self.tickets.pop(0)
-        space = self.parking_spaces.pop(0)
-        print(f'Your ticket number is {ticket} and your parking space is {space} ')
-    else:
-        print("Sorry, the garage is full.")
-
-def Pay_for_parking(self):
-        ticket = int(input("Enter your ticket number: "))
-        if ticket in self.current_ticket:
-            if not self.current_ticket[ticket]['paid']:
-                payment = input("Please enter payment amount: ")
-                if payment:
-                    self.current_ticket[ticket]['paid'] = True
-                else:
-                    print("Your ticket has been paid.  You have 15 minutes to leave, or we will have to use excessive force")
-            else:
-                 print ("The ticket has been paid. ")
+    def Take_ticket(self):
+        if len(self.tickets) >0 and len(self.parking_spaces) > 0:
+            ticket = self.tickets.pop(0)
+            space = self.parking_spaces.pop(0)
+            print(f'Your ticket number is {ticket} and your parking space is {space} ')
         else:
-                 print("Wrong ticket number.")                
-       
-def Leave_garage(self):
-     ticket = int(input("We need your ticket number to exit: "))
+            print("Sorry, the garage is full.")
 
-     if ticket in self.current_ticket:
-          if self.current_ticket[ticket]['paid']:
-               print("Thank you, have a wonderful day. ")
-               del self.current_ticket[ticket]
-               self.tickets.append(ticket)
-               self.parking_spaces.append(ticket)
-     else:
-          print("Wrong ticket number! I'll wait.....")
+    def Pay_for_parking(self):
+            ticket = int(input("Enter your ticket number: "))
+            if ticket in self.current_ticket:
+                if not self.current_ticket[ticket]['paid']:
+                    payment = input("Please enter payment amount: ")
+                    if payment:
+                        self.current_ticket[ticket]['paid'] = True
+                    else:
+                        print("Your ticket has been paid.  You have 15 minutes to leave, or we will have to use excessive force")
+                else:
+                    print ("The ticket has been paid. ")
+            else:
+                    print("Wrong ticket number.")                
+        
+    def Leave_garage(self):
+        ticket = int(input("We need your ticket number to exit: "))
+
+        if ticket in self.current_ticket:
+            if self.current_ticket[ticket]['paid']:
+                print("Thank you, have a wonderful day. ")
+                del self.current_ticket[ticket]
+                self.tickets.append(ticket)
+                self.parking_spaces.append(ticket)
+        else:
+            print("Wrong ticket number! I'll wait.....")
+
+
+garage = ParkingGarage()
+
+garage.take_ticket()
+garage.take_ticket()
+garage.pay_for_parking()
+garage.leave_garage()
